4210b19909d5502496ca78ac8ac3404dc72cb4b7
Meg Alapati <abmfocus4@gmail.com>
2021-07-24 10:15:29 -0400

    aco-tsp working

---
+++ main.py
@@ -0,0 +1,294 @@
+import tsp
+import random
+import math
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+# todo: report
+# todo: graphs
+# todo: max_iterations change
+# todo: something with quiver arrows
+# todo: create README.md
+
+
+def get_best_tour(ants, weights):
+    best = calculate_cost(ants[0], weights)
+    best_ant = 0
+
+    # find the ant with the best tour so far & cost of tour
+    for k in range(1, len(ants)):
+        current = calculate_cost(ants[k], weights)
+        if current < best:
+            best = current
+            best_ant = k
+
+    return ants[best_ant]
+
+
+def calculate_cost(tour, weights):
+    cost = 0
+    for i in range(len(tour) - 1):
+        cost += weights[tour[i]][tour[i + 1]]
+    return cost
+
+
+def online_update(ants, pheromones, weights):
+    try:
+        for i in range(len(pheromones)):
+            for j in range(i + 1, len(pheromones[i])):
+                for k in range(len(ants)):
+                    # decrease pheromone decay constant
+                    pheromones[i][j] *= (1 - rho)
+                    # add new pheromone concentration if path is visited
+                    if visited_path(i, j, ants[k]):
+                        pheromones[i][j] += q / calculate_cost(ants[k], weights)
+                    # pheromone is always equal or greater than decay constant
+                    if pheromones[i][j] < rho:
+                        pheromones[i][j] = rho
+
+                # adjacency preserve
+                pheromones[j][i] = pheromones[i][j]
+    except:
+        return
+
+
+def visited_path(node1, node2, edges):
+    try:
+        node1_idx = edges.index(node1)
+        node2_idx = edges.index(node2)
+    except:
+        node1_idx = 0
+        node2_idx = 0
+
+        last_node = len(edges) - 1
+    # check if edge connected two nodes is visited
+    if (node1_idx - 1 == node2_idx) or (node1_idx + 1 == node2_idx) or (node1_idx == 0 and node2_idx == last_node) \
+            or (node2_idx == 0 and node1_idx == last_node):
+        return True
+    else:
+        return False
+
+
+def offline_update(ants, pheromones, weights):
+    for i in range(len(pheromones)):
+        for j in range(len(pheromones[i])):
+            for k in range(population):
+                pheromones[i][j] = (pheromones[i][j] * (1 - rho)) + (q / calculate_cost(ants[k], weights)) * rho
+                # preserve adjacency
+                pheromones[j][i] = pheromones[i][j]
+
+
+# return a random tour with 29 cities
+def get_tour(start_city, pheromones, weights):
+    tour = [0 for _ in range(num_cities)]
+    visited = [False for k in range(num_cities)]
+
+    tour[0] = start_city
+    if start_city < num_cities:
+        visited[start_city] = True
+
+    for i in range(last_city):
+        next_city = tour[i]
+        _next = select_next(next_city, visited, pheromones, weights)
+        tour[i + 1] = _next
+        visited[_next] = True
+
+    return tour
+
+
+def select_next(city, visited, pheromones, weights):
+    probability = transition_prob(city, pheromones, weights, visited)
+    cum_prob = [0 for _ in range(len(probability) + 1)]
+
+    # calculate cumulative probability from of each city
+    for i in range(len(probability)):
+        cum_prob[i + 1] = cum_prob[i] + probability[i]
+
+    # finding the next city to visit within the range of probability
+    random_fraction = random.random()
+    for j in range(len(cum_prob) - 1):
+        if cum_prob[j] <= random_fraction < cum_prob[j + 1]:
+            return j
+
+
+def transition_prob(city, pheromones, weights, visited):
+    probability = [0 for _ in range(num_cities)]
+    total = 0
+
+    for i in range(len(probability)):
+        # already visited - don't want to visit again
+        if i == city or visited[i]:
+            probability[i] = 0
+        # apply transition rule
+        else:
+            if city < len(pheromones) and i < len(pheromones[city]):
+                probability[i] = math.pow(pheromones[city][i], alpha) * math.pow((1 / weights[i][city]), beta)
+
+        # calculate total probability
+        total += probability[i]
+
+    balanced_prob = [0 for _ in range(num_cities)]
+    # update probabilities with total
+    for i in range(num_cities):
+        if total != 0:
+            balanced_prob[i] = probability[i] / total
+
+    return balanced_prob
+
+
+def print_output(tour, cost, iteration):
+    print('Best Route Found by ACO for Bays29:')
+    print(tour)
+    print('Cost of Best Route is ' + str(cost) + ' found at Iteration ' + str(iteration) + ' by an Ant')
+
+    # creating Map of tour
+    coordinates = [cities.bays29_coordinates[tour[i]] for i in range(len(tour))]
+
+    x = np.array([x[0] for x in coordinates])
+    y = np.array([y[1] for y in coordinates])
+
+    plt.figure()
+    plt.quiver(x[:-1], y[:-1], x[1:] - x[:-1], y[1:] - y[:-1], scale_units='xy', angles='xy', scale=1, color='b')
+
+    plt.xlabel('X-Coordinate')
+    plt.ylabel('Y-Coordinate')
+    plt.title('Best Tour')
+
+    plt.savefig('best_tour')
+
+    print('')
+    print('Tour Map saved as best_tour.png')
+    #
+    #
+    # coordinates.reverse()
+    #
+    # x = np.array([x[0] for x in coordinates])
+    # y = np.array([y[1] for y in coordinates])
+    #
+    # plt.figure()
+    # plt.quiver(x[:-1], y[:-1], x[1:] - x[:-1], y[1:] - y[:-1], scale_units='xy', angles='xy', scale=1, color='b')
+    #
+    # plt.xlabel('X-Coordinate2')
+    # plt.ylabel('Y-Coordinate2')
+    # plt.title('Best Tour2')
+    #
+    # plt.savefig('best_tour2')
+
+    #
+    #
+    plt.figure()
+    plt.scatter(x, y)
+    plt.plot(x, y)
+    for i in range(len(x)):
+        city_num = tour[i] + 1
+        plt.annotate((city_num, x[i], y[i]), (x[i], y[i]))
+    plt.xlabel('X-Coordinate2')
+    plt.ylabel('Y-Coordinate2')
+    plt.title('Best Tour2')
+    plt.savefig('best_tour3')
+
+    print('')
+    print(coordinates)
+    print(tour)
+
+
+if __name__ == '__main__':
+    # bays29 parameters
+    cities = tsp.TSP()
+    weights = cities.calculate_weights()
+    num_cities = cities.num_cities
+    last_city = num_cities - 1
+
+    # aco parameters
+    print('Ant Colony Optimization - TSP Bays29')
+    print('')
+
+    default = int(input('Enter 1 to run ACO with default parameters and 0 to input custom parameters'))
+    print('')
+
+    if default:
+        population = 15
+        rho = 0.5
+        alpha = 1
+        beta = 5
+        q = 2
+        max_iterations = 100
+        online = True
+        # max_iterations = 500
+    else:
+        population = int(input('Enter population of ant colony:'))
+        rho = float(input('Enter pheromone decay constant value:'))
+        alpha = float(input('Enter value of alpha: '))
+        beta = float(input('Enter value of beta: '))
+        q = float(input('Enter value of Q: '))
+        online = bool(input('Enter 1 for enabling and 0 for disabling online pheromone update'))
+        max_iterations = int(input('Enter the number of iterations ACO needs to run for: '))
+        print('')
+
+    print('Running ACO with the following parameters: ')
+    print('')
+
+    print('population: ' + str(population))
+    print('pheromone decay constant: ' + str(rho))
+    print('online pheromone update: ' + str(online))
+    print('alpha: ' + str(alpha))
+    print('beta: ' + str(beta))
+    print('Q: ' + str(q))
+    print('max iterations: ' + str(max_iterations))
+    print('')
+
+    print('Ant Colony Construction in Progress...')
+    print('')
+
+    ants = [0 for _ in range(population)]
+    pheromones = [[0 for _ in range(num_cities)] for y in range(num_cities)]
+
+    # initialize ant k's tour with random cities
+    for k in range(population):
+        ants[k] = []
+        for i in range(num_cities):
+            # get random city not in tour
+            city = random.randint(0, last_city)
+            while city in ants[k]:
+                city = random.randint(0, last_city)
+            # add city to tour
+            ants[k].append(city)
+
+    # set pheromone values to default
+    for i in range(num_cities):
+        for j in range(num_cities):
+            pheromones[i][j] = rho
+
+    best_tour = get_best_tour(ants, weights)
+    best_cost = calculate_cost(best_tour, weights)
+
+    print('Initial Tour Cost: ' + str(best_cost))
+
+    iterations = 0
+    best_iteration = 0
+
+    while iterations < max_iterations:
+        if iterations % 100 == 0 and iterations:
+            print("Iterations Completed: " + str(iterations))
+
+        if online:
+            online_update(ants, pheromones, weights)
+        offline_update(ants, pheromones, weights)
+
+        for i in range(len(ants)):
+            ants[i] = get_tour(ants[i][0], pheromones, weights)
+
+        new_best_tour = get_best_tour(ants, weights)
+        new_best_cost = calculate_cost(new_best_tour, weights)
+
+        if new_best_cost < best_cost:
+            best_cost = new_best_cost
+            best_tour = new_best_tour
+            print('Cost of Best Route so far: ' + str(best_cost))
+            best_iteration = iterations
+
+        iterations += 1
+
+    print('')
+    print_output(best_tour, best_cost, best_iteration)
---
+++ tsp.py
@@ -0,0 +1,62 @@
+import math
+
+
+class TSP:
+    def __init__(self):
+        super().__init__()
+
+        self.bays29_coordinates = [
+            (1150.0, 1760.0),  # 1
+            (630.0, 1660.0),  # 2
+            (40.0, 2090.0),  # 3
+            (750.0, 1100.0),  # 4
+            (750.0, 2030.0),  # 5
+            (1030.0, 2070.0),  # 6
+            (1650.0, 650.0),  # 7
+            (1490.0, 1630.0),  # 8
+            (790.0, 2260.0),  # 9
+            (710.0, 1310.0),  # 10
+            (840.0, 550.0),  # 11
+            (1170.0, 2300.0),  # 12
+            (970.0, 1340.0),  # 13
+            (510.0, 700.0),  # 14
+            (750.0, 900.0),  # 15
+            (1280.0, 1200.0),  # 16
+            (230.0, 590.0),  # 17
+            (460.0, 860.0),  # 18
+            (1040.0, 950.0),  # 19
+            (590.0, 1390.0),  # 20
+            (830.0, 1770.0),  # 21
+            (490.0, 500.0),  # 22
+            (1840.0, 1240.0),  # 23
+            (1260.0, 1500.0),  # 24
+            (1280.0, 790.0),  # 25
+            (490.0, 2130.0),  # 26
+            (1460.0, 1420.0),  # 27
+            (1260.0, 1910.0),  # 28
+            (360.0, 1980.0)  # 29
+        ]
+
+        self.num_cities = 29
+
+    def calculate_weights(self):
+        # initialize weights array
+        weights = [[0 for x in range(self.num_cities)] for y in range(self.num_cities)]
+
+        for i in range(self.num_cities):
+            for j in range(self.num_cities):
+                weights[j][j] = 0
+
+                # coordinates
+                x1 = self.bays29_coordinates[i][0]
+                y1 = self.bays29_coordinates[i][1]
+
+                x2 = self.bays29_coordinates[j][0]
+                y2 = self.bays29_coordinates[j][1]
+
+                # calculate euclidean distances
+                weights[i][j] = math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2, 2))
+
+                # weights from city i to j is the same as the weight of city j to i
+                weights[j][i] = weights[i][j]
+        return weights
